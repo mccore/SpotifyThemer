3a6004f1ebb1965022a72ea055f32aa100cad37a
mccore <edwardmccorry@aol.com>
2016-08-05 15:46:23 -0400

    added prelim icon theming code

--- spotify_themer.sh
+++ spotify_themer.sh
@@ -2,8 +2,8 @@
 
 #TODO:
 #1) up arrow in charts should reain unchanged
-#2) add a usage flag (done)
-#3) add more options for changing colors
+#2) compile .png files into .ico
+#3) add more options for changing colors (in progress)
 #4) add comments to code (done)
 #5) allow people to just enter color options on the command line (done)
 	#-this can be done by displaying options and letting people choose or by outputting choices in help flag (I like his option best)
@@ -27,6 +27,7 @@ if [[ "$1" == "-h" || "$1" == "help" || "$1" == "--help" ]]; then
     		echo ${key} "=" ${COLORS[${key}]}
 	done
 		echo "Use -hex or hex as first argument and then a hex code as the second argument to use your own color"
+		echo "Note: Changed icons look terrible. This is being worked on. As of now it is recommended not to change them."
 	exit
 fi
 
@@ -34,6 +35,8 @@ fi
 if [ ${COLORS[$1]} ]; then
 	choice=${COLORS[$1]}
 elif [[ "$1" == "-hex" || "$1" == "hex" ]]; then
+	choice=$2
+elif [[ "$1" == "-i" || "$1" == "icons" ]]; then
 	choice=$2
 else
 	echo "Not a choice! Exiting..."
@@ -76,3 +79,20 @@ for file in /usr/share/spotify/Apps/*; do
 		rm -rf $no_extension;
 	fi
 done
+
+mkdir /usr/share/spotify/icons/.backups;
+for file in /usr/share/spotify/icons/*; do
+	cp $file /usr/share/spotify/icons/.backups;
+done
+
+if [[ "$1" == "-i" || "$1" == "icons" ]]; then
+	convert spotify-linux-512.png -fuzz 47.102% -fill "#$choice" -opaque "#1ED760" spotify-linux-512.png
+	convert spotify-linux-256.png -fuzz 47.102% -fill "#$choice" -opaque "#1ED760" spotify-linux-256.png
+	convert spotify-linux-128.png -fuzz 40% -fill "#$choice" -opaque "#1ED760" spotify-linux-128.png
+	convert spotify-linux-64.png -fuzz 40% -fill "#$choice" -opaque "#1ED760" spotify-linux-64.png
+	convert spotify-linux-48.png -fuzz 36% -fill "#$choice" -opaque "#1ED760" spotify-linux-48.png
+	convert spotify-linux-32.png -fuzz 34% -fill "#$choice" -opaque "#1ED760" spotify-linux-32.png
+	convert spotify-linux-24.png -fuzz 30% -fill "#$choice" -opaque "#1ED760" spotify-linux-24.png
+	convert spotify-linux-22.png -fuzz 30% -fill "#$choice" -opaque "#1ED760" spotify-linux-22.png
+	convert spotify-linux-16.png -fuzz 22% -fill "#$choice" -opaque "#1ED760" spotify-linux-16.png
+fi
